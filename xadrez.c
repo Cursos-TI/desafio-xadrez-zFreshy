0fb5bd976acba1a11abb05aa6999b00af36780a8
zFreshy <matheusvinni109@gmail.com>
2026-03-12 14:14:41 -0300

    feat: nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -4,35 +4,67 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-    int movimentoTorre, movimentoBispo = 1, movimentoRainha = 1, movimentoCavalo;
+void movimentoTorre(int passo) {
+    if (passo > 5) {
+        return;
+    }
 
-    for (movimentoTorre = 1; movimentoTorre <= 5; movimentoTorre++) {
+    for (int i = 1; i <= 1; i++) {
         printf("a torre foi pra a direita\n");
     }
 
-    printf("\n");
+    movimentoTorre(passo + 1);
+}
 
-    while (movimentoBispo <= 5) {
-        printf("o bispo foi para cima\n");
-        printf("o bispo foi para a direita\n");
-        movimentoBispo++;
+void movimentoBispo(int passo) {
+
+    if (passo > 5) {
+        return;
     }
 
-    printf("\n");
+    for (int i = 1; i <= 1; i++) {
+        printf("o bispo foi para cima\n");
 
-    do {
+        for (int j = 1; j <= 1; j++) {
+            printf("o bispo foi para a direita\n");
+        }
+    }
+
+    movimentoBispo(passo + 1);
+}
+
+void movimentoRainha(int passo) {
+
+    if (passo > 8) {
+        return;
+    }
+
+    for (int i = 1; i <= 1; i++) {
         printf("a rainha foi pra a esquerda\n");
-        movimentoRainha++;
-    } while (movimentoRainha <= 8);
+    }
+
+    movimentoRainha(passo + 1);
+}
+
+int main() {
+
+    movimentoTorre(1);
 
     printf("\n");
 
-     for (movimentoCavalo = 1; movimentoCavalo <= 2; movimentoCavalo++) {
-        printf("o cavalo foi pra baixo\n");
-        while (movimentoCavalo == 2) {
-            printf("o cavalo foi pra esquerda\n");
-            break;
+    movimentoBispo(1);
+
+    printf("\n");
+
+    movimentoRainha(1);
+
+    printf("\n");
+
+     for (int i = 0, j = 4; i <= j; i++, j--) {
+        if (i == j) {
+            printf("o cavalo foi pra cima\n");
+        } else {
+            printf("o cavalo foi pra a direita\n");
         }
     }
 

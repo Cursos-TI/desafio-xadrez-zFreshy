06257f4ef1f2aee98cefb09614bb7d13a7a5b1ac
zFreshy <matheusvinni109@gmail.com>
2026-03-12 13:42:47 -0300

    feat: nivel aventureiro

--- xadrez.c
+++ xadrez.c
@@ -5,11 +5,13 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    int movimentoTorre, movimentoBispo = 1, movimentoRainha = 1;
+    int movimentoTorre, movimentoBispo = 1, movimentoRainha = 1, movimentoCavalo;
 
     for (movimentoTorre = 1; movimentoTorre <= 5; movimentoTorre++) {
         printf("a torre foi pra a direita\n");
     }
+
+    printf("\n");
 
     while (movimentoBispo <= 5) {
         printf("o bispo foi para cima\n");
@@ -17,10 +19,22 @@ int main() {
         movimentoBispo++;
     }
 
+    printf("\n");
+
     do {
         printf("a rainha foi pra a esquerda\n");
         movimentoRainha++;
     } while (movimentoRainha <= 8);
+
+    printf("\n");
+
+     for (movimentoCavalo = 1; movimentoCavalo <= 2; movimentoCavalo++) {
+        printf("o cavalo foi pra baixo\n");
+        while (movimentoCavalo == 2) {
+            printf("o cavalo foi pra esquerda\n");
+            break;
+        }
+    }
 
     return 0;
 }
